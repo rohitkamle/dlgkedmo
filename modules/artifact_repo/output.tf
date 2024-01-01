78a31d8820da3c161946b9289eb11724a782a3f5
Rohit Kamle <rktf2020@gmail.com>
2024-01-01 12:06:15 +0400

    updated

--- modules/artifact_repo/output.tf
+++ modules/artifact_repo/output.tf
@@ -6,3 +6,6 @@ output "name" {
   value = google_artifact_registry_repository.artifact_repo.name
 }
 
+output "repository_url" {
+  value = google_artifact_registry_repository.artifact_repo.repository_url
+}
--- output.tf
+++ output.tf
@@ -6,7 +6,7 @@ output "gke_nodegroup_name" {
   value = module.gke_nodegroup.name
 }
 
-output "artifact_repo_name" {
-  value = module.artifact_repo.name
+output "artifact_repo_url" {
+  value = module.artifact_repo.repository_url
 }
 
